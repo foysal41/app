d05601a93ce33c2c71c116b84b74234ce08722a3
Foysal <mdfoysal.mf33@gmail.com>
2024-04-03 22:06:14 +0600

    added new function transition history

--- bank.js
+++ bank.js
@@ -1,37 +1,45 @@
 class BankAccount {
-  constructor (accountNumber , accountHolder , balance){
+  constructor(accountNumber, accountHolder, balance) {
     this.accountNumber = accountNumber;
     this.accountHolder = accountHolder;
     this.balance = balance;
+    this.transactions = []; // Array to store transaction history
   }
 
-  recharge(amount){
+  recharge(amount) {
     this.balance += amount;
-    console.log(`Your account loaded ${amount}`);
+    this.transactions.push({ type: "deposit", amount, date: new Date() });
+    document.write(`Your account loaded ${amount}`);
   }
 
-  withdwrew(amount){
-    if(this.balance <= 0){
-      console.log(`You current balance is zero please Load `)
-    }else{
+  withdraw(amount) {
+    if (this.balance < amount) {
+      document.write(`Insufficient balance. Please load your account.`);
+    } else {
       this.balance -= amount;
+      this.transactions.push({ type: "withdrawal", amount, date: new Date() });
     }
   }
 
-  getResult(){
+  getResult() {
     return this.balance;
   }
 
-  viewFullINformation(){
-    console.log(`You Account Number is: ${this.accountNumber}`);
-    console.log(`You Name is: ${this.accountHolder}`);
-    console.log(`Your Curren Balance Is $${this.balance}`);
+  viewFullInformation() {
+    document.write(`<br> You Account Number is: ${this.accountNumber} <br>`);
+    document.write(`You Name is: ${this.accountHolder} <br> `);
+    document.write(`Your Current Balance Is $${this.balance} <br>`);
+
+    // Display transaction history
+    document.write("<br> Transaction History: <br>");
+    this.transactions.forEach(transaction => {
+      document.write(`Type: ${transaction.type}, Amount: ${transaction.amount}, Date: ${transaction.date} <br>`);
+    });
   }
 }
 
-
-const account_one = new BankAccount(1001 , "Foysal Jaman" , 1500);
-
+const account_one = new BankAccount(1001, "Foysal Jaman", 1500);
 
 account_one.recharge(200);
-account_one.viewFullINformation();
+account_one.withdraw(100);
+account_one.viewFullInformation();
